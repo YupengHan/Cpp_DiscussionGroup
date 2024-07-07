60204d8255d111fd867bc0ee5012be0a6349f623
YupengHan <hanyupeng9406@gmail.com>
2024-07-07 15:38:28 -0700

    delete myvec.cpp, add basic constructor destructor

---
+++ TemplateContainer/main.cpp
@@ -0,0 +1,14 @@
+#include "myvec.hpp"
+
+int main()
+{
+  MyVec<int> vec_int;
+  // vec_int.push_back(1);
+  // vec_int.push_back(2);
+  // vec_int.push_back(3);
+
+  // for (size_t i = 0; i < vec_int.size(); ++i) {
+  //   std::cout << vec_int[i] << std::endl;
+  // }
+  return 0;
+}
--- TemplateContainer/myvec.hpp
+++ TemplateContainer/myvec.hpp
@@ -5,9 +5,14 @@
 #include <iostream>
 
 template <typename T>
-MyVec
+class MyVec
 {
 private:
+  T *data = nullptr;
+  size_t data_size = 0;
+  size_t data_length = 0;
+  std::vector<T> debug_data = std::vector<T>();
+
 public:
   // constructor
   MyVec();
@@ -17,18 +22,45 @@ public:
   // MyVec(MyVec &&rhs);
   // desctructor
   ~MyVec();
-  void clear();
-  void resize();
-  void push_back();
-  void pop_back();
-  void insert();
-  bool empty() const;
-  size_t size() const;
+  // void clear();
+  // void resize();
+  // void push_back();
+  // void pop_back();
+  // void insert();
+  // bool empty() const;
+  // size_t size() const;
 
   // operator
-  T &operator[](size_t index) const;
-  T &at(size_t index) const;
+  // T &operator[](size_t index) const;
+  // T &at(size_t index) const;
 };
 
+#include "myvec.hpp"
+
+template <typename T>
+MyVec<T>::MyVec()
+{
+  std::cout << "MyVec constructor" << std::endl;
+  data_size = 0;
+  data_length = 0;
+  if (data != nullptr)
+  {
+    delete[] data;
+    data = nullptr;
+  }
+};
+template <typename T>
+MyVec<T>::~MyVec()
+{
+  std::cout << "MyVec destructor" << std::endl;
+  if (data != nullptr)
+  {
+    delete[] data;
+  }
+};
+
+template class MyVec<int>;
 #endif // MYVEC_HPP
-template MyVec<int>;
+// template class MyVec<float>;
+// template class MyVec<double>;
+// template class MyVec<std::string>;
